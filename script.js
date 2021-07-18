8d922160b15e7db2cac4ddba3feaab7e6b8f2a32
Randy Ng <randy.ng@hotmail.sg>
2021-07-19 02:35:30 +0800

    Add mouseover function to grid, buttons and header

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const container = document.getElementById("grid");
-var cell = document.createElement("div");
+const container = document.getElementById('grid');
+var cell = document.createElement('div');
 cell.classList.add('cell');
 for (let i = 0; i < 16*16; i++) {
     container.appendChild(cell.cloneNode(true));
 }
+
+var cells = document.querySelectorAll('.cell')
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = "grey"
+    })
+})
