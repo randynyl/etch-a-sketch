3ac6311f8e99e1d5bd97afbccf6ecec74b218433
Randy Ng <randy.ng@hotmail.sg>
2021-07-19 15:01:16 +0800

    Add clear grid function

--- script.js
+++ script.js
@@ -8,6 +8,36 @@ for (let i = 0; i < 16*16; i++) {
 var cells = document.querySelectorAll('.cell')
 cells.forEach((cell) => {
     cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = "grey"
+        cell.classList.add('hovered')
     })
 })
+
+const clearBtn = document.getElementById('clear-button');
+clearBtn.addEventListener('click', () => {
+    console.log('clear button clicked');
+    cells.forEach((cell) => {
+        cell.classList.remove('hovered');
+    })
+})
+
+const gridSizeBtn = document.getElementById('grid-size-button');
+gridSizeBtn.addEventListener('click', () => {
+    console.log('size button clicked');
+    rowSize = promptRowSize();
+    console.log(rowSize);
+
+})
+
+function promptRowSize() {    
+    do {
+        let rowSizeInput = prompt('How many squares per side in the grid? (1-100)');
+        if (rowSizeInput == null || rowSizeInput == '') {
+            return -1;
+        }
+        else {
+            console.log(rowSizeInput);
+            let rowSize =  parseInt(rowSizeInput);
+        }
+    } while (rowSize < 1 || rowSize > 100 || isNaN(rowSize))
+    return rowSize;
+}
